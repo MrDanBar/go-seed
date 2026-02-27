edb476324cb9968cc2ef47f8c5d96c32b466b511
MrDanBar <daniel.barrientosLopez@gmail.com>
2026-02-27 14:49:16 -0600

    feat: why fmt.ErrorF

--- hello/about-me.go
+++ hello/about-me.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
@@ -10,4 +13,21 @@ func main() {
 	text3 := "."
 
 	fmt.Println(text, text2, text3)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter a text: ")
+
+	value, _ := reader.ReadString('\n')
+
+	fmt.Println(value)
+
+	number, error := strconv.ParseFloat(value, 64)
+
+	if error != nil {
+		myError := fmt.Errorf("Houston tenemos problemas.. %v", value)
+		fmt.Println("e:", myError)
+	}
+
+	fmt.Println("Number is:", number)
 }
