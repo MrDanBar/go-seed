10f0dbaf816bcb6da59294e07c69c8bab8a9f9b6
MrDanBar <daniel.barrientosLopez@gmail.com>
2026-03-03 09:44:28 -0600

    feat: capital letter

--- hello/server-test.go
+++ hello/server-test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -8,15 +9,18 @@ import (
 
 var saludos = []string{"konichiwa", "101", "hi", "hola", "ciao", "salute"}
 
+const WorkerNuber = 5
+
 func main() {
-	http.HandleFunc("/v1/test", MyTest)
+	http.HandleFunc("/v1/tests", MyTests)
+	http.HandleFunc("/v1/tests/{id}", MyTestById)
 
 	fmt.Println("Listening on port 8080...")
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func MyTest(writer http.ResponseWriter, request *http.Request) {
+func MyTests(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintln(writer, "My first test (CTF)", "More", "Text")
 
 	channel := make(chan string)
@@ -40,4 +44,29 @@ func Saludar(channel chan<- string) {
 	}
 
 	close(channel)
+}
+
+func MyTestById(writer http.ResponseWriter, request *http.Request) {
+	// fmt.Fprintf(writer, request.Pattern)
+
+	// writer.WriteHeader(404)
+	// writer.Write([]byte("H G O L A"))
+
+	myFirstTest := Test{"T-5195a1sd9", "mrdanbar", 178035106510, []string{"Astrid san wa doko desu ka"}}
+
+	writer.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(writer).Encode(myFirstTest)
+
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+type Test struct {
+	Id          string   `json:"id"`
+	AuthorId    string   `json:"author_id"`
+	TimeCreated int64    `json:"time_created"`
+	Questions   []string `json:"questions"`
 }
