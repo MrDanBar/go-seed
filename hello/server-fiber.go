12e53b383a6f9d9e8077c4b11af8a221f98ad3ed
MrDanBar <daniel.barrientosLopez@gmail.com>
2026-03-03 21:02:04 -0600

    feat: fiber+json

--- hello/server-fiber.go
+++ hello/server-fiber.go
@@ -12,7 +12,7 @@ func main() {
 	myTestApp.Get("/v1/tests", func(context *fiber.Ctx) error {
 		testSample := Test2{"T-987894166519", "Sample of a good name", 36519819819514}
 
-		return context.SendString(testSample.Id)
+		return context.JSON(testSample)
 	})
 
 	log.Println("HOLA")
@@ -21,7 +21,7 @@ func main() {
 }
 
 type Test2 struct {
-	Id          string `json: id`
-	Name        string `json: name`
-	createdTime int64  `json: time_created`
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	CreatedTime int64  `json:"time_created"`
 }
